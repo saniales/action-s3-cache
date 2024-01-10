0f84803e7cd171c4aa22f5d2396481d42ca949ce
Alessandro Sanino <3513298+saniales@users.noreply.github.com>
2024-01-10 18:59:49 +0000

    refactor: :recycle: minor refactor

--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -4,7 +4,7 @@ import { Outputs, RefKey, State } from "../constants";
 
 export function isGhes(): boolean {
     const ghUrl = new URL(
-        process.env["GITHUB_SERVER_URL"] || "https://github.com"
+        process.env.GITHUB_SERVER_URL || "https://github.com"
     );
     return ghUrl.hostname.toUpperCase() !== "GITHUB.COM";
 }
